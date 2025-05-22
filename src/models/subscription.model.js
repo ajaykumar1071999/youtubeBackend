e5c0e39dcf7115f5657f66186626a22d704c16e4
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2025-05-22 06:50:15 +0000

    feat: add Subscription schema with amount and subscriber/channel references

---
+++ src/models/subscription.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
